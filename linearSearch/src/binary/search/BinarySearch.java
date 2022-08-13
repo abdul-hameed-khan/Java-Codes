65029b3e1ab79261acbb262458769912e3e7d209
Abdul Hameed Khan <abdulhameed314khan@gmail.com>
2022-08-13 09:54:45 +0500

    Add files via upload

---
+++ linearSearch/src/binary/search/BinarySearch.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binary.search;
+
+/**
+ *
+ * @author Zulfiqar
+ */
+public class BinarySearch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] a = new int[]{2,1,5,8,6,5,4,9,12,65,45};
+        int l=0;
+        int[] b={1,2,4,8,5,5};
+        int m=4;
+        int n=a.length-1;
+        while(m<n)
+        {
+        if()    
+        }
+    }
+    
+}
